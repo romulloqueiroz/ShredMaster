3e767e0bf6660ba877c6a34e6e9cf4397ab17cf9
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-03-25 15:35:58 +0900

    Icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -7,23 +7,26 @@ const Icon: React.FC<Partial<IconProps>> = ({
   name = 'play',
   color = 'white',
   size = 48,
-}) => (
-  <View w={size} h={size}>
-    <Canvas style={{ flex: 1 }}>
-      <FitBox
-        src={rect(0, 0, 10, 12)}
-        dst={rect(0, 0, size, size)}
-      >
-        {icon[name].map((d: string) => (
-          <Path
-            key={d}
-            path={d}
-            color={colors[color]}
-          />
-        ))}
-      </FitBox>
-    </Canvas>
-  </View>
-)
+}) => {
+  const [vw, vh, ...path] = icon[name]
+  return (
+    <View w={size} h={size}>
+      <Canvas style={{ flex: 1 }}>
+        <FitBox
+          src={rect(0, 0, vw, vh)}
+          dst={rect(0, 0, size, size)}
+        >
+          {path.map((d: string) => (
+            <Path
+              key={d}
+              path={d}
+              color={colors[color]}
+            />
+          ))}
+        </FitBox>
+      </Canvas>
+    </View>
+  )
+}
 
 export default Icon
