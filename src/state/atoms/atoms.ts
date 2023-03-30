72b99953c31034e3d0bb042c974c885ed1b8398d
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-03-30 15:16:34 +0900

    Exercises atoms init

--- src/state/atoms/atoms.ts
+++ src/state/atoms/atoms.ts
@@ -1,44 +1,17 @@
 import { atom } from 'recoil'
 
-// Atoms for tabata timer parameters
-const playTimeAtom = atom({
-  key: 'playTimeAtom',
-  default: 20, // default play time in seconds
-})
 
-const restTimeAtom = atom({
-  key: 'restTimeAtom',
-  default: 10, // default rest time in seconds
-})
 
-// Atoms for metronome parameters
-const bpmAtom = atom({
-  key: 'bpmAtom',
-  default: 120, // default beats per minute
-})
+export type Exercise = {
+  id: number;
+  name: string;
+};
 
-const activeAtom = atom({
-  key: 'activeAtom',
-  default: false, // metronome is initially inactive
-})
-
-const exercisesState = atom({
+const exercisesState = atom<Exercise[]>({
   key: 'exercises',
   default: [],
 })
 
-const metronomeState = atom({
-  key: 'metronome',
-  default: {
-    isPlaying: false,
-  },
-})
-
 export {
-  playTimeAtom,
-  restTimeAtom,
-  bpmAtom,
-  activeAtom,
   exercisesState,
-  metronomeState,
 }
--- src/state/selectors/selectors.ts
+++ src/state/selectors/selectors.ts
@@ -1,14 +1,17 @@
-import { selector } from 'recoil'
-import { activeAtom, restTimeAtom, playTimeAtom } from '../atoms/atoms'
+import { DefaultValue, selector } from 'recoil'
+import { Exercise, exercisesState } from '../atoms/atoms'
+import { SecureStore } from '@helpers'
 
-// Selector to determine if the tabata timer is in "play" mode
-export const isPlayingSelector = selector({
-  key: 'isPlayingSelector',
-  get: ({ get }) => {
-    const active = get(activeAtom)
-    const rest = get(restTimeAtom)
-    const play = get(playTimeAtom)
-
-    return active && rest < play
+export const addExerciseSelector = selector<Exercise | null>({
+  key: 'updateExercises',
+  get: () => null,
+  set: ({ set }, newExercise) => {
+    if (newExercise === null || newExercise instanceof DefaultValue) return
+    set(exercisesState, (currentExercises) => {
+      if (currentExercises instanceof DefaultValue) return [newExercise]
+      const updatedExercises = [...currentExercises, newExercise]
+      SecureStore.create('exercises', updatedExercises)
+      return updatedExercises
+    })
   },
 })
