fad269b0e6680948ed19ed62806efe5f82265d76
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-04-03 11:42:39 +0900

    Fonts loaded

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
+import { useFonts } from 'expo-font'
 import { Navigation } from '@navigation'
 import { RecoilRoot } from 'recoil'
 
-const App = () => (
-  <RecoilRoot>
-    <Navigation />
-  </RecoilRoot>
-)
+const useLoading = () => {
+  const [fontsLoaded] = useFonts({
+    FilsonProBold: require('./src/assets/fonts/FilsonProBold.otf'),
+    FilsonProMedium: require('./src/assets/fonts/FilsonProMedium.otf'),
+    FilsonProRegular: require('./src/assets/fonts/FilsonProRegular.otf'),
+  })
+
+  if (!fontsLoaded) return false
+  return fontsLoaded
+}
+
+
+const App = () => {
+  const isLoadingComplete = useLoading()
+
+  return (
+    <>
+      {isLoadingComplete && (
+        <RecoilRoot>
+          <Navigation />
+        </RecoilRoot>
+      )}
+    </>
+  )
+}
 
 export default App
