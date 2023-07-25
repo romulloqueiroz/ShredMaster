b033111c35b3da3cfb34ac2cb8f0fafb1e054f66
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-07-25 17:04:07 +0900

    removing test

--- src/navigation/routes/Routes.tsx
+++ src/navigation/routes/Routes.tsx
@@ -2,14 +2,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { RoutesList } from './Routes.types'
 import { BottomTabNavigator } from './BottomTabNavigator'
 import { Tabata, Timer } from '@screens'
-import Test from './Test/Test'
 
 const Stack = createNativeStackNavigator<RoutesList>()
 
 const Routes = () => (
   <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
-    <Stack.Screen name='Root' component={Test} />
-    {/* <Stack.Screen name='Root' component={BottomTabNavigator} /> */}
+    <Stack.Screen name='Root' component={BottomTabNavigator} />
     <Stack.Screen name='Tabata' component={Tabata} />
     <Stack.Screen name='Timer' component={Timer} />
   </Stack.Navigator>
