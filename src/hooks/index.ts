4db5d516bd0e21e33027105ae348d4b8f3d37c10
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-09-12 18:43:46 +0900

    organizing imports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,25 +1,25 @@
-import { useRoute } from './useRoute/useRoute'
-import { useStreak } from './useStreak/useStreak'
-import { useTabata } from './useTabata/useTabata'
 import { useCountdown } from './useCountdown/useCountdown'
-import { useSimpleCountdown } from './useSimpleCountdown/useSimpleCountdown'
 import { useExercises } from './useExercises/useExercises'
 import { useLoadExercises } from './useLoadExercises/useLoadExercises'
 import { useMetronome } from './useMetronome/useMetronome'
+// import { useMetronomeState } from './useMetronomeState/useMetronomeState'
 import { useNameValidation } from './useNameValidation/useNameValidation'
 import { useNavigation } from './useNavigation/useNavigation'
-// import { useMetronomeState } from './useMetronomeState/useMetronomeState'
+import { useRoute } from './useRoute/useRoute'
+import { useSimpleCountdown } from './useSimpleCountdown/useSimpleCountdown'
+import { useStreak } from './useStreak/useStreak'
+import { useTabata } from './useTabata/useTabata'
 
 export {
-  useRoute,
-  useStreak,
-  useTabata,
   useCountdown,
-  useSimpleCountdown,
   useExercises,
   useLoadExercises,
   useMetronome,
+  // useMetronomeState,
   useNameValidation,
   useNavigation,
-  // useMetronomeState,
+  useRoute,
+  useSimpleCountdown,
+  useStreak,
+  useTabata,
 }
