5a7166f0ec9ad9daacc4fbf7ddbc9e4a999e16dc
Romullo Queiroz de Assis Bernardo <romulloqueiroz@hotmail.com>
2023-09-12 18:44:00 +0900

    useSuccessSound hook - init

---
+++ src/hooks/useSuccessSound/useSuccessSound.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef } from 'react'
+import { Audio } from 'expo-av'
+
+export const useSuccessSound = () => {
+  const [successSoundLoaded, setSuccessSoundLoaded] = useState(false)
+  const successSound = useRef(new Audio.Sound())
+
+  const successSoundSource = require('../../assets/success_sound.mp3')
+
+  const loadSuccessSound = async () => {
+    try {
+      await successSound.current.loadAsync(successSoundSource)
+      setSuccessSoundLoaded(true)
+    } catch (error) {
+      console.error('Error loading success sound:', error)
+    }
+  }
+
+  useEffect(() => {
+    loadSuccessSound()
+
+    return () => {
+      if (successSoundLoaded) {
+        successSound.current.unloadAsync()
+      }
+    }
+  }, [])
+
+  const playSuccessSound = async () => {
+    if (successSoundLoaded) {
+      try {
+        await successSound.current.replayAsync()
+      } catch (error) {
+        console.error('Error playing success sound:', error)
+      }
+    }
+  }
+
+  return playSuccessSound
+}
